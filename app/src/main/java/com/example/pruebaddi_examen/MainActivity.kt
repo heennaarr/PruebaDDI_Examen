0475ee5aa1a86ab51d881f14385be77bddd3082c
heennaarr <henar.herher@educa.jcyl.es>
2024-11-21 11:38:24 +0100

    todo con boton editText

--- app/src/main/java/com/example/pruebaddi_examen/MainActivity.kt
+++ app/src/main/java/com/example/pruebaddi_examen/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.pruebaddi_examen
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.silla_botones)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.silla_botones) // Asegúrate de que el layout correcto esté cargado
+
+        val editText = findViewById<EditText>(R.id.edit)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button1 = findViewById<Button>(R.id.button)
+        val textView1 = findViewById<TextView>(R.id.text1)
+        val textView2 = findViewById<TextView>(R.id.text2)
+
+        button2.setOnClickListener {
+            // Mostrar el Toast cuando se presione el botón
+            Toast.makeText(applicationContext, "¡Has presionado el botón!", Toast.LENGTH_SHORT).show()
         }
-    }
+        button1.setOnClickListener{
+            val inputText = editText.text.toString()
+            if (inputText.isNotEmpty()) {
+                textView1.text = inputText
+                textView2.text = inputText
+            }
+        }    }
 }
